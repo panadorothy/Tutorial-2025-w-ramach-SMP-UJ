b93bdba7713c36fb780c3e9f7f43eea2bc2e5512
Daria Panaiotova <dasapanaetova6@gmail.com>
2025-12-05 10:46:03 +0300

    sentiment_facebook.py

--- nltk_project/sentiment_facebook.py
+++ nltk_project/sentiment_facebook.py
@@ -17,8 +17,20 @@ def analyze_sentiment(text):
 # Example
 # ---------------------------
 if __name__ == "__main__":
-    sample_text = "For everyone with negativity, do you think these young people asked for a war . Britian has fought many wars we didnt vote or ask for and many lives were lost in the process . This isnt a joke or time to ridicule people who are suffering. I would hope and pray for safety if anything happened in the UK . We are lucky enough to never see bombs drop on our homes and experience such trauma . I wish you both all the best"
+    sample_text = "text"
     sentiment = analyze_sentiment(sample_text)
 
     print("Text:", sample_text)
     print("Sentiment Scores:", sentiment)
+
+# ---------------------------
+# Komentarz pozytywny:
+# Great idea! Thanks for sharing! What a great way to show off those awesome stamps!!
+# ---------------------------
+# ---------------------------
+# Komentarz negatywny:
+# 
+# ---------------------------
+# Komentarz mieszany:
+# 
+# ---------------------------
